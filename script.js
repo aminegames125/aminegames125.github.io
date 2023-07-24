5776ecc9c9fccf89eaf38700d7fbf8e054e44671
aminegames125 <73359408+aminegames125@users.noreply.github.com>
2023-07-24 14:40:43 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Theme Toggle Function
-document.getElementById("themeToggle").addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-  const currentTheme = document.body.classList.contains("dark-mode") ? "Light Theme" : "Dark Theme";
-  document.getElementById("themeToggle").textContent = currentTheme;
-});
